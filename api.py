c0b22b2b6cd005f42e04ccb1c5a2b86c5273d380
Alena Papko <alena.papko@outlook.com>
2024-04-30 19:19:14 +0300

    correct

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ class Pets:
         self.base_url = 'http://34.141.58.52:8000/'
 
     def get_token(self) -> json:
-        """Запрос к Swagger сайта для получения уникального токена по указанным email и password"""
+        """Запрос к Swagger сайта для получения токена"""
         data = {"email": settings.VALID_EMAIL,
                 "password": settings.VALID_PASSWORD}
         res = requests.post(self.base_url + 'login', data=json.dumps(data))
@@ -24,7 +24,7 @@ class Pets:
 
 
     def get_list_users(self):
-        """Запрос к Swagger сайта для получения id пользователя по указанным email и password"""
+        """Запрос к Swagger сайта для получения id пользователя"""
         my_token = Pets().get_token()[0]
         headers = {'Authorization': f'Bearer {my_token}'}
         res = requests.get(self.base_url + 'users', headers=headers)
@@ -38,19 +38,19 @@ class Pets:
         my_id = Pets().get_token()[2]
         headers = {'Authorization': f'Bearer {my_token}'}
         data = {"id": 0,
-                "name": 'Bela', "type": 'dog', "age": 2, "owner_id": my_id}
+                "name": 'Ruffu', "type": 'dog', "age": 9, "owner_id": my_id}
         res = requests.post(self.base_url + 'pet', data=json.dumps(data), headers=headers)
         pet_id = res.json()['id']
         status = res.status_code
         return pet_id, status
 
     def upload_pet_photo(self):
-        """Запрос к Swagger сайта для загрузки фотографии к карточке созданного питомца зарегистрированным пользователем"""
+        """Запрос к Swagger сайта для загрузки фотографии к аккаунту созданного питомца зарегистрированным пользователем"""
         my_token = Pets().get_token()[0]
         pet_id = Pets().create_pet()[0]
         headers = {'Authorization': f'Bearer {my_token}'}
-        files = {'pic': ('viz.jpg', open('tests\\Photo\\viz.jpg', 'rb'), 'image/jpg')}
-        #pic = open('tests\\Photo\\viz.jpg', 'rb')
+        files = {'pic': ('Ruffy.jpg', open('tests\\Photo\\Ruffy.jpg', 'rb'), 'image/jpg')}
+        #pic = open('tests\\Photo\\Ruffy.jpg', 'rb')
         #files = {pic: pic}
 
         res = requests.post(self.base_url + f'pet/{pet_id}/image', headers=headers, files=files)
@@ -97,7 +97,7 @@ class Pets:
         my_id = Pets().get_token()[2]
         pet_id = Pets().post_pet()[1]
         headers = {'Authorization': f'Bearer {my_token}'}
-        data = {"id": pet_id, "type": "dog", "name": 'Alex', "owner_id": my_id}
+        data = {"id": pet_id, "type": "dog", "name": 'Zhora', "owner_id": my_id}
         res = requests.patch(self.base_url + 'pet', data=json.dumps(data), headers=headers)
         status = res.status_code
         return status
@@ -109,7 +109,7 @@ class Pets:
         my_id = Pets().get_token()[2]
         pet_id = 1547
         headers = {'Authorization': f'Bearer {my_token}'}
-        data = {"id": pet_id, "type": "dog", "name": 'Alex2', "owner_id": my_id}
+        data = {"id": pet_id, "type": "dog", "name": 'Zhoraa', "owner_id": my_id}
         res = requests.patch(self.base_url + 'pet', data=json.dumps(data), headers=headers)
         status = res.status_code
         return status
@@ -140,7 +140,7 @@ class Pets:
         my_id = Pets().get_token()[2]
         headers = {'Authorization': f'Bearer {my_token}'}
         data = {"id": 0,
-                "name": 'Bela', "type": 'dog', "age": 2, "owner_id": my_id}
+                "name": 'Ruffy', "type": 'dog', "age": 9, "owner_id": my_id}
         res = requests.post(self.base_url + 'pet', data=json.dumps(data), headers=headers)
         status = res.status_code
         return status
